f3774b026b0dba023bd1e1261758fc1b04552357
kierepkae <kierepkaedyta@gmail.com>
2019-07-09 11:15:43 +0100

    add tests to library_spec.rb and Library class in library.rb

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,1 +1,37 @@
 require 'spec_helper.rb'
+
+describe "Library"do
+  before :all do
+    lib_arr = [
+      Book.new("JavaScript: The Good Parts", "Douglas Crocford", :development),
+      Book.new("Designing with Web Standards", "Jeffrey Zeldman", :design),
+      Book.new("Don't Make me Think", "Steve Krug", :usability),
+      Book.new("JavaScrip Patterns","Stoyan Stefanov", :development),
+      Book.new("Responsive Web Design", "Ethan Marcotte", :design),
+    ]
+
+    File.open "books.yml", "w" do |f|
+      f.write YAML::dump lib_arr
+    end
+  end
+
+  before :each do
+    @lib = Library.new "books.yml"
+  end
+
+  describe "#new" do
+    context "with no parameters" do
+      it "has no books" do
+        lib = Library.new
+        lib.books.length.should == 0
+
+      end
+    end
+
+    context "with yaml file name parameter" do
+      it "has five books" do
+        @lib.books.length == 5
+      end
+    end
+  end
+end
